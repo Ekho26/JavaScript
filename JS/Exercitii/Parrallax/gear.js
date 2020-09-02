403ee3d8d65960a3e53998ab90c8baef32e3c105
Ekho26 <munteanu.ionut1991@gmail.com>
2020-09-02 19:21:06 +0300

    portfolio

---
+++ JS/Exercitii/Parrallax/gear.js
@@ -0,0 +1,10 @@
+document
+        .querySelector('.container')
+        .addEventListener('scroll', rotate);
+function rotate() {
+    let distance = document
+                .querySelector('.container')
+                .scrollTop;
+    document.querySelector('.gear img')
+            .style.transform = `rotate(${distance}deg)`;
+}
