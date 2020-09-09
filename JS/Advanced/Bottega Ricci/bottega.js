8847bba2a7ce6b8081ecbf2e9754b3f691840507
Ekho26 <munteanu.ionut1991@gmail.com>
2020-09-09 13:20:41 +0300

    Js advanced

---
+++ JS/Advanced/Bottega Ricci/bottega.js
@@ -0,0 +1,25 @@
+function spanTitle() {
+    let title = document.querySelector('.title');
+    let text = title.innerHTML.trim();
+    let letters = text.split('');
+    let spans = letters.map(
+        function(letter){
+            let elem =  document.createElement('span');
+            elem.innerText = letter;
+            return elem;
+        }    
+    );
+    title.innerHTML = '';
+    spans.forEach(span => title.appendChild(span));
+    return spans;
+}
+function shuffle() {
+    
+}
+
+let spans = spanTitle();
+spans.sort(function (){
+    return 0.5 - Math.random()
+});
+let spansToAnimate = spans.slice(0, 3);
+spansToAnimate.forEach(span => span.classList.add('bounce'));
