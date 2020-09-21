2948692d4b337cbf342da2e48ca49f0585f3f5dc
Ekho26 <munteanu.ionut1991@gmail.com>
2020-09-21 21:06:28 +0300

    TypeScript

---
+++ JS/Advanced/Exercitii/TypeScript/test.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+    return a + b;
+}
+sum('x', 'y');
+var res = sum(10, 20);
+console.log(res);
---
+++ JS/Advanced/Exercitii/TypeScript/test.ts
@@ -0,0 +1,9 @@
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+// sum('x', 'y');
+
+let res = sum(10, 20);
+
+console.log(res);
