e31ef46c533d4d668d3d711c323a4a3cab1ea9b4
Ekho26 <munteanu.ionut1991@gmail.com>
2020-09-26 17:11:05 +0300

    todoAppReact

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import Todo from './Todo';
+import db from './firebase';
+import firebase from 'firebase';
 
 function App() {
+  const [todos,setTodos] = useState([]);
+  const [input,setInput] = useState('');
+
+  useEffect(() => {
+      db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+        setTodos(snapshot.docs.map(doc => doc.data().todo))
+      })
+  }, [])
+
+  const addTodo = (event) => {
+    // this will fire off when we click the button
+    event.preventDefault();
+
+    db.collection('todos').add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp() 
+    })
+
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To Do</h1>
+      <form>
+        <FormControl>
+          <InputLabel>To do</InputLabel>
+          <Input value={input} onChange={event => setInput(event.target.value)}/>
+        </FormControl>
+        <Button disabled={!input} type="submit" onClick={addTodo} variant="contained" color="primary">
+          Add Todo
+        </Button>
+      </form>
+      <ul>
+        {todos.map(todo => (
+          <Todo todo={todo}/>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ todo/src/Todo.js
@@ -0,0 +1,22 @@
+import {List, ListItem, ListItemText} from '@material-ui/core';
+import React from 'react';
+import './Todo.css';
+import db from './firebase';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+
+function Todo(props) {
+    return (
+        <List className='todoList'>
+            <ListItem>
+                <ListItemText primary={props.todo} secondary="" />
+            </ListItem>
+            <DeleteForeverIcon onClick={
+                event => db.collection('todos')
+                           .doc(props.todo.id)
+                           .delete()}
+            />
+        </List>
+    )
+}
+
+export default Todo;
