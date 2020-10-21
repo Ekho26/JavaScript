155a8886d61578f221b4318bca8af7ee26778beb
Ekho26 <munteanu.ionut1991@gmail.com>
2020-10-21 10:13:56 +0300

    Todo-project update

--- todo-project/src/Todo.js
+++ todo-project/src/Todo.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './Todo.css';
-import {List, ListItem, ListItemAvatar, ListItemText} from '@material-ui/core'
+import {List, ListItem, ListItemText} from '@material-ui/core'
 
 
 function Todo(props) {
     return (
         <List className='todo_list'>
             <ListItem>
-                <ListItemAvatar>
-                </ListItemAvatar>
             <ListItemText primary = {props.text} secondary = 'Hurry Up!'/>
             </ListItem>
         </List>
