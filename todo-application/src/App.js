c9b05b06d870b4f9a56ee84fa1589364f2c3a822
Ekho26 <munteanu.ionut1991@gmail.com>
2020-10-23 21:17:01 +0300

    todo-project almost done

--- todo-application/src/App.js
+++ todo-application/src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
+import Todo from './Todo.js';
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core';
+import LibraryAddCheckTwoToneIcon from '@material-ui/icons/LibraryAddCheckTwoTone';
 import './App.css';
+import firebase from 'firebase';
+import db from './firebase';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  useEffect(() =>{
+    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot =>{
+      setTodos(snapshot.docs.map(doc => ({id: doc.id, todo: doc.data().todo})))
+    })
+  }, []);
+
+  const addTodo =(event) =>{
+    event.preventDefault();
+    db.collection('todos').add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    setInput('');
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        ToDo
+      </h1>
+      <form>
+        <FormControl>
+          <InputLabel>Add a task</InputLabel>
+          <Input value={input} 
+                 onChange={event => setInput(event.target.value)} />
+        </FormControl>
+        <Button disabled= {!input} 
+                type='submit' 
+                onClick={addTodo} 
+                variant="contained" 
+                color="primary">
+          <LibraryAddCheckTwoToneIcon />
+        </Button>
+      </form>
+      <ul>
+        {todos.map(todo => (
+          <Todo todo={todo} />
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
---
+++ todo-application/src/Todo.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, List, ListItem, ListItemText, Modal } from '@material-ui/core';
+import DeleteForeverTwoToneIcon from '@material-ui/icons/DeleteForeverTwoTone';
+import ListAltTwoToneIcon from '@material-ui/icons/ListAltTwoTone';
+import CheckBoxTwoToneIcon from '@material-ui/icons/CheckBoxTwoTone';
+import './Todo.css';
+import db from './firebase';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+
+function Todo(props) {
+    const classes = useStyles();
+    const [open, setOpen] =useState(false);
+    const [input, setInput] = useState('');
+
+    const handleOpen = () =>{
+        setOpen(true);
+    };
+
+    const updateTodo = () => {
+        db.collection('todos').doc(props.todo.id).set({
+            todo: input
+        }, { merge: true })
+        setOpen(false);
+    }
+
+    return (
+        <>
+        <Modal 
+            open={open}
+            onClose={e => setOpen(false)}>
+                <div className={classes.paper}>
+                    <h1>Edit</h1>
+                    <input placeholder={props.todo.todo} value={input} onChange={event => setInput(event.target.value)}/>
+                    <Button onClick= {updateTodo}>
+                        <CheckBoxTwoToneIcon />
+                    </Button>
+                </div>
+        </Modal>
+        
+        <List className='todo_list'>
+            <ListItem>
+                <ListItemText primary={props.todo.todo} secondary='Todo'/>
+            </ListItem>
+            <Button onClick={e => setOpen(true)}>
+                <ListAltTwoToneIcon />
+            </Button>
+            <Button onClick={event => db.collection('todos')
+                                        .doc(props.todo.id)
+                                        .delete()}
+            >
+                <DeleteForeverTwoToneIcon />
+            </Button>
+        </List>
+        </>
+    )
+};
+
+export default Todo;
