6643d0faadc0822cfb5e0348a83ecdd8340bba20
Ekho26 <munteanu.ionut1991@gmail.com>
2020-10-23 11:30:13 +0300

    updates at todo-project

--- Todo-project/todo/src/App.js
+++ Todo-project/todo/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+
+
   return (
     <div className="App">
-
+      <h1>Todo</h1>
+      <input />
+      <button>Add todo</button>
+      <ul>
+        {todos.map(todo => (
+        <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
