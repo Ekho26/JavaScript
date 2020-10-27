4e412b1ded8b7f8a5c585d38e2a65d6bfaf102f1
Ekho26 <munteanu.ionut1991@gmail.com>
2020-10-27 14:30:59 +0200

    Adding Dark mode

--- todo-application/src/App.js
+++ todo-application/src/App.js
@@ -1,54 +1,94 @@
-import React, { useState, useEffect} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import InputComponent from './components/InputComponent.js';
-import TodoListComponent from './components/TodoListComponent.js';
-import HeaderComponent from './components/HeaderComponent';
-import Container from '@material-ui/core/Container';
-import CSSBaseline from '@material-ui/core/CssBaseline';
-import db from './firebase.config';
-import uuid from 'uuid';
-
-const useStyles = makeStyles((theme) => ({
-  container: {
-    marginTop: 30,
-    width: 600,
-    backgroundColor: theme.palette.background.paper,
-    boxShadow: theme.shadows[5],
-    borderRadius: 8,
-    padding: theme.spacing(2, 4, 3),
-  }
-}));
+import React, { useState, useEffect } from "react";
+import InputComponent from "./components/InputComponent.js";
+import TodoListComponent from "./components/TodoListComponent.js";
+import HeaderComponent from "./components/HeaderComponent";
+import Container from "@material-ui/core/Container";
+import CSSBaseline from "@material-ui/core/CssBaseline";
+import Switch from "@material-ui/core/Switch";
+import { blue, indigo, grey } from "@material-ui/core/colors";
+import Brightness7Icon from "@material-ui/icons/Brightness7";
+import Brightness2Icon from "@material-ui/icons/Brightness2";
+import {
+  createMuiTheme,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import db from "./firebase.config";
+import uuid from "uuid";
 
 function App() {
-  const classes = useStyles();
   const [todos, setTodos] = useState([]);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  useEffect(() =>{
-    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot =>{
-      setTodos(snapshot.docs.map(doc => ({id: doc.id, todo: doc.data().todo})))
-    })
+  const useStyles = makeStyles((theme) => ({
+    container: {
+      marginTop: 30,
+      width: 600,
+      background: isDarkMode ? grey[400] : grey[100],
+      color: theme.palette.getContrastText(isDarkMode ? grey[400] : grey[100]),
+      boxShadow: theme.shadows[5],
+      borderRadius: 8,
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+
+  const classes = useStyles();
+
+  let theme = createMuiTheme({
+    pallete: {
+      type: isDarkMode ? "dark" : "light",
+      primary: {
+        main: isDarkMode ? blue[900] : indigo[900]
+      },
+      secondary: {
+        main: isDarkMode ? blue[200] : blue[200]
+      },
+      background: {
+        default: isDarkMode ? grey[200] : grey[100]
+      }
+    },
+  });
+
+  useEffect(() => {
+    db.collection("todos")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setTodos(
+          snapshot.docs.map((doc) => ({ id: doc.id, todo: doc.data().todo }))
+        );
+      });
   }, []);
 
   return (
     <>
       <CSSBaseline />
-      <Container maxWidth="sm"
-                 aligncontent="center"
-                 className={classes.container}
-                 key={uuid()}
-                 >
-        <HeaderComponent key={uuid()}/>
-        <InputComponent/>
-          <div>
-            {todos.map(todo => (
-              <TodoListComponent 
-                todo={todo}
-                key={uuid()}/>
-            ))}
+      <ThemeProvider theme={theme}>
+        <Container
+          maxWidth="sm"
+          aligncontent="center"
+          className={classes.container}
+          key={uuid()}
+        >
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Brightness7Icon />
+            <Switch checked ={isDarkMode}
+                    onClick={() => setIsDarkMode(mode=>!mode)}/>
+            <Brightness2Icon />
           </div>
-      </Container>
+          <HeaderComponent key={uuid()} />
+          <InputComponent />
+          {todos.map((todo) => (
+            <TodoListComponent todo={todo} key={uuid()} />
+          ))}
+        </Container>
+      </ThemeProvider>
     </>
   );
-};
+}
 
 export default App;
--- todo-application/src/components/InputComponent.js
+++ todo-application/src/components/InputComponent.js
@@ -1,53 +1,62 @@
-import React, {useState} from 'react';
-import { Button, FormControl, InputLabel, Input, Grid } from '@material-ui/core';
-import LibraryAddCheckTwoToneIcon from '@material-ui/icons/LibraryAddCheckTwoTone';
-import firebase from 'firebase';
-import db from '../firebase.config';
+import React, { useState } from "react";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  Input,
+  Grid,
+} from "@material-ui/core";
+import LibraryAddCheckTwoToneIcon from "@material-ui/icons/LibraryAddCheckTwoTone";
+import firebase from "firebase";
+import db from "../firebase.config";
 
 function InputComponent() {
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
 
-  const handleSubmit =(event) =>{
+  const handleSubmit = (event) => {
     event.preventDefault();
     // if(inputValue === '') return;
-    db.collection('todos').add({
+    db.collection("todos").add({
       todo: inputValue,
       timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-    })
-    setInputValue('');
+    });
+    setInputValue("");
   };
 
-  const handleChange =(event) =>{
-    setInputValue(event.target.value)
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
   };
 
-    return (
-        <>
-            <form>
-                <Grid container 
-                      alignItems='center'
-                      justify='space-around'
-                      style={{marginBottom:'3em'}}>
-                    <Grid item>
-                        <FormControl>
-                            <InputLabel>Add a task</InputLabel>
-                            <Input value={inputValue} 
-                                   onChange={handleChange} />
-                        </FormControl>
-                    </Grid>
-                    <Grid item>
-                        <Button type='submit' 
-                                disabled={!inputValue}
-                                onClick={handleSubmit} 
-                                variant="contained" 
-                                color="primary">
-                            <LibraryAddCheckTwoToneIcon />
-                        </Button>
-                    </Grid>
-                </Grid>
-            </form>
-        </>
-    )
+  return (
+    <>
+      <form>
+        <Grid
+          container
+          alignItems="center"
+          justify="space-around"
+          style={{ marginBottom: "3em" }}
+        >
+          <Grid item>
+            <FormControl color='secondary'>
+              <InputLabel color='secondary'>Add a task</InputLabel>
+              <Input value={inputValue} onChange={handleChange} />
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <Button
+              type="submit"
+              disabled={!inputValue}
+              onClick={handleSubmit}
+              variant="contained"
+              color="primary"
+            >
+              <LibraryAddCheckTwoToneIcon />
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </>
+  );
 }
 
 export default InputComponent;
