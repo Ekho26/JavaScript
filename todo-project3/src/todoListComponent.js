c73caddb72d1ae6c8e5c45bcf884ac789490b4ba
Ekho26 <munteanu.ionut1991@gmail.com>
2021-02-11 18:22:06 +0200

    changes

--- todo-project3/src/todoListComponent.js
+++ todo-project3/src/todoListComponent.js
@@ -1,79 +1,92 @@
 import React from "react";
-import { 
+import {
   IconButton,
   ButtonGroup,
   Chip,
-  Grid, 
-  Paper, 
-  Typography} from '@material-ui/core';
-  import DeleteIcon from "@material-ui/icons/Delete";
-  import EditIcon from "@material-ui/icons/Edit";
-  import CheckIcon from "@material-ui/icons/Check";
-  import {CSSTransition, TransitionGroup} from "react-transition-group";
+  Grid,
+  Paper,
+  Typography,
+} from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
+import CheckIcon from "@material-ui/icons/Check";
+import Zoom from '@material-ui/core/Zoom';
 
-  const getTextDecor = (done) => {
-    return (done? 'line-throguh': 'none')
-  }
+const getTextDecor = (done) => {
+  return done ? "line-throguh" : "none";
+};
 
 function TodoListComponent(props) {
   return (
-    <TransitionGroup>
-    <Grid container direction="column" spacing={2}
-          style={{marginTop: '1.5em'}}>
-      {props.todos.map(todo => {
-        return (
-          <CSSTransition key={todo.id}
-            timeout={400}>
-          <Grid item>
-            <Paper style={{padding: '0.8em'}} elevation={4}>
-              <Grid container 
-                    alignItems="center"
-                    justify="space-between">
-                <Grid item>
-                  <Typography variant="h6"
-                              style={{textDecoration: todo.done? "line-through":"none"}}>
-                    {todo.val}
-                  </Typography>
-                </Grid>
-                <Grid>
-                  <Chip size="small"
+      <Grid
+        container
+        direction="column"
+        spacing={2}
+        style={{ marginTop: "1.5em" }}
+      >
+        {props.todos.map((todo) => {
+          return (
+            <Zoom in={true} style={{transitionDelay: '200ms'}}>
+              <Grid item key={todo.id}>
+                <Paper style={{ padding: "0.8em" }} elevation={4}>
+                  <Grid container alignItems="center" justify="space-between">
+                    <Grid item>
+                      <Typography
+                        variant="h6"
+                        style={{
+                          textDecoration: todo.done ? "line-through" : "none",
+                        }}
+                      >
+                        {todo.val}
+                      </Typography>
+                    </Grid>
+                    <Grid>
+                      <Chip
+                        size="small"
                         color="primary"
                         label={todo.priority}
                         onClick={() => {
-                          props.handlePriorityClick(todo.priority)
-                        }}/>
-                </Grid>
+                          props.handlePriorityClick(todo.priority);
+                        }}
+                      />
+                    </Grid>
+                  </Grid>
+                  <Typography variant="body2">Due: {todo.dueDate}</Typography>
+                  <ButtonGroup
+                    color="primary"
+                    aria-label="outlined primary button group"
+                    variant="text"
+                    size="small"
+                    style={{ paddingTop: "12px" }}
+                  >
+                    <IconButton>
+                      <EditIcon
+                        onClick={() => {
+                          props.handleEditClick(todo);
+                        }}
+                      />
+                    </IconButton>
+                    <IconButton>
+                      <CheckIcon
+                        onClick={() => {
+                          props.handleMarkDone(todo);
+                        }}
+                      />
+                    </IconButton>
+                    <IconButton>
+                      <DeleteIcon
+                        onClick={() => {
+                          props.handleDelete(todo.id);
+                        }}
+                      />
+                    </IconButton>
+                  </ButtonGroup>
+                </Paper>
               </Grid>
-              <Typography variant="body2">
-                Due: {todo.dueDate}
-              </Typography>
-              <ButtonGroup color="primary" 
-                           aria-label="outlined primary button group"
-                           variant="text"
-                           size="small"
-                           style={{paddingTop: "12px"}}>
-                <IconButton>
-                  <EditIcon onClick={() => {
-                    props.handleEditClick(todo)}}/>
-                </IconButton >
-                <IconButton>
-                  <CheckIcon onClick={() => {
-                    props.handleMarkDone(todo)
-                  }}/>
-                </IconButton >
-                <IconButton>
-                  <DeleteIcon onClick={() => {
-                    props.handleDelete(todo.id)
-                  }}/>
-                </IconButton >
-              </ButtonGroup>
-            </Paper>
-          </Grid>
-          </CSSTransition>
-        );
-      })}
-    </Grid>
-    </TransitionGroup>
+            </Zoom>
+          );
+        })}
+      </Grid>
   );
 }
 
