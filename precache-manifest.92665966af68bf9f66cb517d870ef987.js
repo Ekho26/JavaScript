a70e6b9d9f56d960d3d1d5e7aa520883c21f78de
Ekho26 <munteanu.ionut1991@gmail.com>
2021-02-16 19:25:21 +0200

    Updates

---
+++ precache-manifest.92665966af68bf9f66cb517d870ef987.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd5a4b07935e01376d074642c04f54d",
+    "url": "/Todo-project/index.html"
+  },
+  {
+    "revision": "d6dbe252e2e3dba4b975",
+    "url": "/Todo-project/static/js/2.840a64c2.chunk.js"
+  },
+  {
+    "revision": "ffab3168c6f4a51843888f63c352f03e",
+    "url": "/Todo-project/static/js/2.840a64c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50cd760b4952370f5a4b",
+    "url": "/Todo-project/static/js/main.3eaa4cdf.chunk.js"
+  },
+  {
+    "revision": "f14a657bc22799aba738",
+    "url": "/Todo-project/static/js/runtime-main.33bd53f1.js"
+  }
+]);
